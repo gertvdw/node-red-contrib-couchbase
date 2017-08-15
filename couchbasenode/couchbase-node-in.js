80fb9794e0d2586949947bc3589f2338d312d627
Gert van der Weyde <gert@ionline.nl>
2017-08-15 15:15:04 +0200

    createNode

--- couchbasenode/couchbase-node-in.js
+++ couchbasenode/couchbase-node-in.js
@@ -5,11 +5,17 @@
 
 module.exports = function(RED) {
     function CouchbaseNode(config) {
+        RED.nodes.createNode(this, config);
         var node = this;
         var couchbase = require('couchbase');
         var cluster = new couchbase.Cluster('couchbase://' + config.server);  // + config.server);
         var bucket = cluster.openBucket(config.bucket, function(err) {
-           if (err) { node.error("Failed to connect to " + config.bucket, err); }
+            if (err) { 
+                node.error("Failed to connect to " + config.bucket, err);
+                node.status({ fill: 'red', shape: 'ring', text: 'disconnected'});
+            } else {
+                node.status({ fill: 'green', shape: 'dot', text: 'connected'});
+            }
         });
         bucket.operationTimeout = 60 * 1000;
         var context = this.context();
@@ -21,10 +27,10 @@ module.exports = function(RED) {
                 this.mybucket.upsert(msg.topic, doc, function (err, result) {
                     if (err) {
                         node.status({fill:'red', shape:'ring'});
-                        console.log(err);
-                        throw err;
+                        node.error(err);
+                    } else {
+                        node.status( { fill:'green', shape:'dot'} );
                     }
-                    node.status( { fill:'green', shape:'dot'} );
                 });
             } catch (err) {
                 node.status( { fill:'red', shape:'ring', text:'timeout'} );
